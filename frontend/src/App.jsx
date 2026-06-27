5b6de9211ef53369249cbc5539cce2a370e21b61
Varun <bavaskarvarun3484@gmail.com>
2026-06-27 20:00:41 +0530

    Implemented Login, About & Contact us Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Orders from './Pages/Orders'
 
 const App = () => {
   return (
@@ -30,7 +31,7 @@ const App = () => {
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/place-order' element={<PlaceOrder/>}/>
-          <Route path='/order' element={<Order/>}/>
+          <Route path='/orders' element={<Orders/>}/>
       </Routes>
       <Footer />
     </div>
