c91ef22d85479f0b156283fb92521613ea2102bc
Varun <bavaskarvarun3484@gmail.com>
2026-06-28 19:35:35 +0530

    Started backend, created product and user models

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectdb from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+//App Config
+const app = express();
+const port = process.env.PORT || 4000
+connectdb()
+connectCloudinary();
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.get('/', (req, res) =>{
+    res.send("API WORKING");
+})
+
+app.listen(port, () => console.log("Server start on PORT: "+ port));
